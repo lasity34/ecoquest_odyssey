5be75cc07f17d143de0cd26331b4ff293f79ad06
Bjorn Worrall <85297118+lasity34@users.noreply.github.com>
2023-12-05 12:49:41 +0200

    nom changes

---
+++ .history/index_20231205124858.js
@@ -0,0 +1,50 @@
+import express from "express";
+import { engine } from 'express-handlebars'
+import dotenv from "dotenv";
+import pgPromise from "pg-promise";
+import session from 'express-session';
+
+
+// login, signup routers
+import signupRouter from "./api/signup-route.js";
+import loginRouter from "./api/login-route.js";
+
+// App instance
+const app = express();
+
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static("public"));
+
+
+app.engine("handlebars", engine({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+app.set("views", "./views");
+
+dotenv.config();
+
+// const connection = {
+// 	connectionString: process.env.ecoquest_odyssey_url,
+// 	ssl: { rejectUnauthorized: false },
+// };
+
+ 
+const pgp = pgPromise();
+
+// const db = pgp(connection);
+
+// Cors middleware
+app.use(cors({
+    origin: "*"
+}));
+
+
+// Routes middlewares
+app.use("/signup", signupRouter);
+app.use("/login", loginRouter);
+
+const PORT = process.env.PORT || 3014;
+
+app.listen(PORT, function () {
+  console.log("App has started", PORT);
+});
