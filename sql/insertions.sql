f825862fa32d1a8e226d46fcccd3aff5a526c0c2
mokhelek <mkatleho191@gmail.com>
2023-12-05 23:01:00 -0500

    add databse tables

---
+++ sql/insertions.sql
@@ -0,0 +1,11 @@
+INSERT INTO quest(title, description, ecosystem_type)
+VALUES
+    ('plants','A quest to find an learn about different types of flowers','cape flowers');
+
+
+INSERT INTO quest_item(title, description, id)
+VALUES
+    ('protea','national flower',1),
+    ('ericas','purple cape flower','1'),
+    ('cape marguerite','white color cape flower','1')
+    ;
--- sql/tables.sql
+++ sql/tables.sql
@@ -4,17 +4,26 @@ CREATE TABLE user_table (
     email VARCHAR(100) UNIQUE,
     password_hash VARCHAR(255) NOT NULL
 );
-CREATE TABLE quests (
-    quest_id SERIAL PRIMARY KEY,
+CREATE TABLE quest(
+    id SERIAL PRIMARY KEY,
     title VARCHAR(100) NOT NULL,
     description TEXT,
     ecosystem_type VARCHAR(50),
-    difficulty_level VARCHAR(50)
 );
-CREATE TABLE user_quests (
-    user_quest_id SERIAL PRIMARY KEY,
+
+CREATE TABLE quest_item (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    description TEXT,
+    quest_id INT,
+    FOREIGN KEY (quest_id) REFERENCES quest(id)
+);
+
+CREATE TABLE user_quest (
+    id SERIAL PRIMARY KEY,
     user_id INTEGER REFERENCES user_table(user_id),
     quest_id INTEGER REFERENCES quests(quest_id),
     progress_status VARCHAR(50) NOT NULL,
     completion_date TIMESTAMP WITH TIME ZONE
 );
+
